86ae05246eac38c4a1cacd49cd62423806c26785
Karl Martin Holst <99191682+kamaho@users.noreply.github.com>
2022-09-26 15:43:31 +0200

    fixed comments

--- Oblig0_Oppgave4.hs
+++ Oblig0_Oppgave4.hs
@@ -1,13 +1,8 @@
 module ObligOppgave4 where
 
-    -- Nå skal vi lage høypassfilteret. Igjen tar vi en snarvei: Lavpassfilteret fjerner
-    -- den høyfrekvente delen av signalet, så hvis vi subtraherer det filtrerte signalet fra
-    -- det orginale, burde vi stå igjen med den høyfrekvente delen som lavpassfilteret
-    -- tok bort.
     -- I denne oppgaven skal du skrive en funksjon for høypassfilter som tar det samme
     -- paramereret som lavpass filteret, og subtraherer verdien i lavpass filteret med
     -- den nyeste verdien til signalet (første element i listen).
-    -- hpf :: (Fractional a) => Integer -> [a] -> a
 
     -- regner ut gjennomsnittet av en liste.
     average :: (Fractional a) => [a] -> a
@@ -17,6 +12,6 @@ module ObligOppgave4 where
     lpf :: Fractional a => Int -> [a] -> a
     lpf x l = average (take x l)
     
-    --hpf :: (Fractional a) => Integer -> [a] -> a
+    -- subtraherer verdien i lavpass filteret med den nyeste verdien til signalet.
     hpf :: Fractional a => Int -> [a] -> a
     hpf z l = (lpf z l) - head(l)
